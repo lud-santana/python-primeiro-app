1b36e1c711f8268077aa029ffa5a5afcf1a9354d
Yuri <yurisilvasantana2011@hotmail.com>
2024-01-14 01:06:37 -0300

    Adicionar formatação ao código

--- aula1/app.py
+++ aula1/app.py
@@ -1,9 +1,11 @@
-print("< Sabor Express >\n")
+print("""
+< 𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤 >
+      """)
 
 print("1. Cadastrar restaurante")
 print("2. Listar restaurante")
 print("3. Ativar restaurante")
 print("4. Sair\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print("Sua escolha foi ", opcao_escolhida)
+opcao_escolhida = input("Escolha uma opção:")
+print(f"Sua escolha foi {opcao_escolhida}.\n")
