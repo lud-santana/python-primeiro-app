3d7f2b41579e14231b044c248e1a31bc5009b546
Yuri <yurisilvasantana2011@hotmail.com>
2024-01-16 18:01:32 -0300

    Começar a desenvolver aula 2

---
+++ aula2/app.py
@@ -0,0 +1,11 @@
+print("""
+< 𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤 >
+      """)
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção:")
+print(f"Sua escolha foi {opcao_escolhida}.\n")
